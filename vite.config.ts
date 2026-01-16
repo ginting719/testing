bc807eea165f93feab644da8c07f58e85b69a2fe
ginting719 <gintingeka.apotekalpro@gmail.com>
2026-01-16 20:42:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // Hardcode the base path for GitHub Pages deployment.
   // This should match your repository name.
-  base: '/receiving-journey/',
+  base: '/testing/',
 })
